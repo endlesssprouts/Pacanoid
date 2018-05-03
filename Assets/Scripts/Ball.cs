5a1933dd8b9b5979a3b0568b4112cd93ed326a9f
Max <maxymkarpinsky@gmail.com>
2018-05-04 02:08:06 +0300

    Values tweaking

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
-    private float speed = 2;
+public class Ball : MonoBehaviour
+{
+	[SerializeField] private Vector3 _initialForce = new Vector3(0f, 1f, 0f);
 
-    void Start()
-    {
-        // Initial Velocity
-        GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private Rigidbody2D _rigidbody2D;
+
+	private void Update()
+	{
+		Debug.Log(this._rigidbody2D.velocity);
+	}
+
+	private void Awake()
+	{
+		this._rigidbody2D = this.GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		this._rigidbody2D.velocity = this._initialForce;
 	}
 }
