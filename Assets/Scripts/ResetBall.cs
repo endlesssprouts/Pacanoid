d44d734db350cac23afe45b19b7089b1fb788184
Kostas Man <anthosnet@hotmail.com>
2018-05-05 21:24:24 +0300

    Collision Boxes, Playable Level
    a) Added Collision Boxes to the maze objects
    b)Added the script for the bricks
    c) TestLevel01 is playable (almost)

---
+++ Assets/Scripts/ResetBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBall : MonoBehaviour {
+
+    [SerializeField] private Transform paddle;
+    private Vector2 position;
+    private float posX;
+    private float posY;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    posX = paddle.transform.position.x;
+    posY = paddle.transform.position.y;
+
+        Debug.Log("X is: " + posX + "Y is: " +posY);
+    
+    }
+}
