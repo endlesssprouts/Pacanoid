a5d6ed87da03dc3af26e293399c4e21c2de55fdc
Kostas Man <anthosnet@hotmail.com>
2018-05-07 01:01:14 +0300

    LevelTest02, ResetBall Script
    a) a new TestLevel02
    b) Simple ResetBall Script for playing non stop
    c)some Polishing

--- Assets/Scripts/ResetBall.cs
+++ Assets/Scripts/ResetBall.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// A script to return the Ball to the Paddle's position
+
 public class ResetBall : MonoBehaviour {
 
     [SerializeField] private Transform paddle;
@@ -9,18 +11,21 @@ public class ResetBall : MonoBehaviour {
     private float posX;
     private float posY;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Awake()
+    {
+        if (paddle == null)
+        {
+            Debug.LogError("Error! Paddle not defined!");
+        }
+    }
 
-    posX = paddle.transform.position.x;
-    posY = paddle.transform.position.y;
-
-        Debug.Log("X is: " + posX + "Y is: " +posY);
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("OutOfBounds") )
+        {
+            posX = paddle.transform.position.x;
+            posY = paddle.transform.position.y;
+            this.transform.position = new  Vector2(posX , posY + 0.1f);
+        }
     }
 }
