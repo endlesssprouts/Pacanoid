4ba63d70c5e6cca62abf82f2dd1d210de45c2c6f
Max <maxymkarpinsky@gmail.com>
2018-05-15 02:14:19 +0300

    Add level manager behaviour for UI elements

---
+++ Assets/Scripts/LevelManagerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class LevelManagerBehaviour : MonoBehaviour
+{
+	public void LoadNextLevel()
+	{
+		LevelManager.LoadNextLevel();
+	}
+
+	public void LoadPreviousLevel()
+	{
+		LevelManager.LoadPreviousLevel();
+	}
+
+#if UNITY_EDITOR
+	//protected override void OnDrawGizmos()
+	//{
+
+	//}
+#endif
+}
